153b62cdc69516dc373b4f7e76fd36965df2a5a7
lorenzboss <lorenz.boss@bbzbl-it.ch>
2024-05-10 09:08:04 +0200

    deleted old package

--- app/src/main/java/ch/bfh/teamulrich/metaldetector/Screen.kt
+++
@@ -1,8 +0,0 @@
-package ch.bfh.teamulrich.metaldetector
-
-import androidx.annotation.DrawableRes
-
-sealed class Screen(val route: String, @DrawableRes val icon: Int) {
-    object Sensor : Screen("Sensor", R.drawable.ic_sensor)
-    object Reader : Screen("Reader", R.drawable.ic_reader)
-}
